67f680ab747066cb692ae81bd23fcd1f1793b02a
Mantvydas <razuleviciusmantvydas@gmail.com>
2018-12-12 15:08:19 +0200

    test2

--- WebApplication1/DbContext/BaseDB.cs
+++ WebApplication1/DbContext/BaseDB.cs
@@ -6,6 +6,5 @@ namespace WebApplication1.DbContext
     {
         public const string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=interactiveTechnologiesDatabase";
        public MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-        //
     }
 }
