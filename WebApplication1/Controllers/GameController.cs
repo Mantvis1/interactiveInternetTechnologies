df6731400108ebe230fbb727542dec68618bf400
Mantvydas <razuleviciusmantvydas@gmail.com>
2018-12-14 11:34:57 +0200

    registration to tournament

--- WebApplication1/Controllers/GameController.cs
+++ WebApplication1/Controllers/GameController.cs
@@ -10,18 +10,32 @@ namespace WebApplication1.Controllers
     public class GameController : Controller
     {
         GameDB game = new GameDB();
-
+        [HttpGet]
         public ActionResult GameSelection()
         {
             int countOfPlayersInTournament = game.CountOfTournamentPlayers();
             ViewBag.Message = countOfPlayersInTournament;
             return View(countOfPlayersInTournament);
         }
-
+        [HttpGet]
         public ActionResult MyTeam()
         {
             
             return View();
+        }
+
+       // [HttpPost]
+        public ActionResult RegisterNewCompetotor()
+        {
+            bool registrationSuccess = game.CreateNewCompetotor((int)Session["id"]);
+            if (registrationSuccess == true)
+            {
+                return RedirectToAction("GameSelection");
+            }
+            else
+            {
+                return RedirectToAction("GameSelection"); // Error message
+            }
         }
     }
 }
--- WebApplication1/DbContext/GameDB.cs
+++ WebApplication1/DbContext/GameDB.cs
@@ -31,5 +31,40 @@ namespace WebApplication1.DbContext
             databaseConnection.Close();
             return count;
         }
+
+        public bool CreateNewCompetotor(int userId)
+        {
+            int isExist = isUserExist(userId);
+            if(isExist == 0)
+            {
+                databaseConnection.Open();
+                query = "INSERT INTO tournament(id, userId) VALUES (NULL," + userId + ")";
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                MySqlDataReader reader = commandDatabase.ExecuteReader();
+                databaseConnection.Close();
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        private int isUserExist(int userId)
+        {
+            int count = 0;
+            databaseConnection.Open(); ;
+            query = "SELECT COUNT(`userId`) FROM `tournament` WHERE userId = " + userId;
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            MySqlDataReader reader = commandDatabase.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                   count = int.Parse(reader.GetString(0));
+                }
+            }
+            databaseConnection.Close();
+            return count;
+        }
     }
 }
