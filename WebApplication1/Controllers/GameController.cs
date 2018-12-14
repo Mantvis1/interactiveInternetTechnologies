d9c71b111ffd69ab4da0d544180c118ad21ec6ad
Mantvydas <razuleviciusmantvydas@gmail.com>
2018-12-14 11:09:54 +0200

    Number of competitors can be seen.

--- WebApplication1/Controllers/GameController.cs
+++ WebApplication1/Controllers/GameController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.DbContext;
 
 namespace WebApplication1.Controllers
 {
     public class GameController : Controller
     {
+        GameDB game = new GameDB();
+
         public ActionResult GameSelection()
         {
-            return View();
+            int countOfPlayersInTournament = game.CountOfTournamentPlayers();
+            ViewBag.Message = countOfPlayersInTournament;
+            return View(countOfPlayersInTournament);
         }
 
         public ActionResult MyTeam()
         {
+            
             return View();
         }
     }
---
+++ WebApplication1/DbContext/GameDB.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.DbContext
+{
+    public class GameDB : BaseDB
+    {
+        string query = "";
+        public GameDB()
+        {
+
+        }
+
+        public int CountOfTournamentPlayers()
+        {
+            int count = 0;
+            databaseConnection.Open();
+            query = "Select COUNT(id) from tournament";
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            MySqlDataReader reader = commandDatabase.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    count = int.Parse(reader.GetString(0));
+                }
+            }
+            databaseConnection.Close();
+            return count;
+        }
+    }
+}
