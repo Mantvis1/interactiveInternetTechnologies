f8bd72b07b711f40743ca0158d38f1de36e63ad5
Mantvydas <razuleviciusmantvydas@gmail.com>
2018-12-27 10:10:37 +0200

    Error messages added in settings page

--- WebApplication1/Controllers/ManageController.cs
+++ WebApplication1/Controllers/ManageController.cs
@@ -1,12 +1,81 @@
 ﻿using System.Web.Mvc;
+using WebApplication1.DbContext;
 
 namespace WebApplication1.Controllers
 {
     public class ManageController : Controller
     {
+        ManageDB MDB = new ManageDB();
+
+        [HttpGet]
         public ActionResult Settings()
         {
+            ViewBag.ErrorMessage = Session["error"];
+            Session["error"] = null;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(string oldPassword, string newPassword, string repeatNewPassword)
+        {
+            if (newPassword == repeatNewPassword)
+            {
+                if (MDB.UpdatePassword() == true)
+                {
+                    Session["error"] = "Sekmingai paskeistas slaptažodis";
+                }
+                else
+                {
+                    Session["error"] = "Klaida pakeičiant slaptažodį";
+                }
+            }else
+            {
+                Session["error"] = "Skiriasi ivesti slaptazodziai";
+            }
+            return RedirectToAction("Settings");
+        }
+
+        [HttpPost]
+        public ActionResult ChangeUsername(string newUserName, string password)
+        {
+            if(MDB.UpdateUsername() == true)
+            {
+                Session["error"] = "Sekmingai paskeistas vartotojo vardas";
+            }
+            else
+            {
+                Session["error"] = "Klaida pakeičiant vartotojo vardą";
+            }
+            return RedirectToAction("Settings");
+        }
+
+        [HttpPost]
+        public ActionResult ChangeEmail(string oldEmail, string newEmail, string password)
+        {
+            if(MDB.UpdateEmail() == true)
+            {
+                Session["error"] = "Sekmingai paskeistas pastas";
+            }
+            else
+            {
+                Session["error"] = "Klaida pakeičiant paštą";
+            }
+            return RedirectToAction("Settings");
+        }
+
+       // [HttpDelete]
+        public ActionResult DeleteAccount()
+        {
+            if (MDB.DeleteAccount() == true)
+            {
+                Session.Clear();
+                return RedirectToAction("About", "Home");
+            } else
+            {
+                Session["error"] = "Nepavyko ištrinti paskyros";
+                return RedirectToAction("Settings");
+            }
+           
         }
     }
 }
---
+++ WebApplication1/DbContext/ManageDB.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication1.DbContext
+{
+    public class ManageDB : BaseDB
+    {
+        string query = "";
+
+        public ManageDB()
+        {
+
+        }
+
+        public bool UpdatePassword()
+        {
+            return true;
+        }
+
+        public bool UpdateUsername()
+        {
+            return true;
+        }
+
+        public bool UpdateEmail()
+        {
+            return true;
+        }
+
+        public bool DeleteAccount()
+        {
+            return false;
+        }
+    }
+}
